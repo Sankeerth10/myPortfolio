2e76c0ee33db3404f32fa0cf6baebea77722b986
sankeerth Koduri <sankeerth.koduri@gmail.com>
2023-05-19 10:55:31 -0400

    webpage bug fix

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ class App extends Component {
 
   getResumeData() {
     $.ajax({
-      url: '/resumeData.json',
+      url: 'myPortfolio/resumeData.json',
       dataType: 'json',
       cache: false,
       success: function (data) {
